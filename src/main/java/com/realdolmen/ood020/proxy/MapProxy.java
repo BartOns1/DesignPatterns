1d48e0d308ec282430ae98ba89557d2a9443d910
Bart Ons <Bart.Ons@telenet.be>
2017-08-16 12:19:25 +0200

    first commit

--- src/main/java/com/realdolmen/ood020/proxy/MapProxy.java
+++ src/main/java/com/realdolmen/ood020/proxy/MapProxy.java
@@ -1,30 +1,50 @@
 package com.realdolmen.ood020.proxy;
 
 import java.util.HashMap;
+import java.util.Properties;
 
 public class MapProxy implements AbstractMap {
 
 	private String fileName;
-	private HashMap<String, String> hashtable = new HashMap<String, String>();
+	private HashMap<String, String> cache = new HashMap<>();
+
+	private Map map;
 
 	public MapProxy(String fileName) {
+		this.fileName = fileName;
 
 	}
 
+
 	public String find(String key) throws Exception {
-		// TODO implement method
-		return null;
+		if (cache.containsKey(key)){
+			return cache.get(key);
+		} else {
+			String value=getMap().find(key);
+			cache.put(key,value);
+			return value;
+		}
+
 	}
 
 	public void add(String key, String value) throws Exception {
+		getMap().add(key,value);
+		cache.put(key,value);
 
 	}
+	private Map getMap(){
+		if (map==null){
+			map = new Map(fileName);
+		}
+		return map;
+	}
+
 
 	private String get(String key) {
-		return (String) hashtable.get(key);
+		return (String) cache.get(key);
 	}
 
 	private void put(String key, String value) {
-		hashtable.put(key, value);
+		cache.put(key, value);
 	}
 }
